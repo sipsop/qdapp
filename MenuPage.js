1bd85586f4cb433902a3c730fbdb7a8cd43feee1
Mark Florisson <mark.florisson@cl.cam.ac.uk>
2016-08-03 10:37:36 +0100

    More work on menu item layout

--- MenuPage.js
+++ MenuPage.js
@@ -4,17 +4,22 @@ import {
   Image,
   StyleSheet,
   Text,
+  TextInput,
   View,
   ScrollView,
   ListView,
+  Picker,
+  // Slider,
 } from 'react-native'
 import Dimensions from 'Dimensions'
 import _ from 'lodash'
 
 // import Carousel from 'react-native-carousel'
 import Carousel from 'react-native-carousel-control'
+import Slider from 'react-native-slider'
 
 import { SizeTracker } from './SizeTracker.js'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export class MenuPage extends SizeTracker {
     /* properties:
@@ -56,16 +61,16 @@ export class MenuPage extends SizeTracker {
 const beerImg = "https://i.kinja-img.com/gawker-media/image/upload/s--neYeJnUZ--/c_fit,fl_progressive,q_80,w_636/zjlpotk0twzrtockzipu.jpg"
 
 var carouselStyles = StyleSheet.create({
-  container: {
-    width: 400,
-    height: 200,
-    // flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: 'transparent',
-    borderWidth: 1,
-    margin: 10,
-  },
+    container: {
+        width: 400,
+        height: 200,
+        // flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: 'transparent',
+        borderWidth: 1,
+        margin: 10,
+    },
 })
 
 
@@ -79,7 +84,6 @@ class MenuItem extends Component {
     render = () => {
         return <View style={menuItemStyle.menuItemView}>
             <PrimaryMenuItem />
-            <Selection />
         </View>
     }
 }
@@ -89,27 +93,54 @@ class PrimaryMenuItem extends Component {
         return <View style={menuItemStyle.primaryMenuItemView}>
             <Image source={{uri: beerImg}} style={menuItemStyle.image} />
             <View style={menuItemStyle.contentView}>
-                <View style={{height: 50}}>
-                    <Text style={menuItemStyle.titleText}>Guiness</Text>
+                <View style={{flex: 5, flexWrap: 'wrap'}}>
+                    <Text lineBreakMode='tail' numberOfLines={2} style={menuItemStyle.titleText}>
+                        Guiness
+                        {/*Rock Bottom Cask Conditioned Bourbon Chocolate Oatmeal Stout*/}
+                    </Text>
                     <Text style={menuItemStyle.keywordText}>
                         #stout #dry #irish
                     </Text>
-                    <Text style={menuItemStyle.infoText}>
+                    <Text style={menuItemStyle.infoText} numberOfLines={5}>
                         Guinness is an Irish dry stout.
                     </Text>
+                    <View style={{flex:1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+                        <Picker selectedValue={"pint"} style={{flex: 1}}>
+                            <Picker.Item value="pint" label="pint" />
+                            <Picker.Item value="half-pint" label="half-pint" />
+                        </Picker>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginLeft: 10}}>
+                            <Icon name="minus-square-o" size={20} style={{marginLeft: 5}} color="#900" />
+                            <TextInput
+                                style={{marginLeft: 5, textAlign: 'center'}}
+                                placeholder="0"
+                                />
+                            <Icon name="plus-square-o" size={20} style={{marginLeft: 5}} color="rgb(16, 145, 17)" />
+                        </View>
+                        <Text style={{textAlign: 'right'}}>£0</Text>
+                     </View>
                 </View>
-                <View style={{height: 50, justifyContent: 'flex-end'}}>
-                    <Prices />
+                <View style={{flex: 2, alignItems: 'center'}}>
+                    <Icon name="heart-o" size={30} color="#900" style={{marginTop: 15}} />
                 </View>
             </View>
         </View>
     }
 }
 
-class Prices extends Component {
+class MenuItemHeader extends Component {
     render = () => {
-        return <View style={menuItemStyle.priceView}>
-            <Text style={menuItemStyle.priceText}>£3.40</Text>
+        return <View style={{flex: 5, flexWrap: 'wrap'}}>
+            <Text lineBreakMode='tail' numberOfLines={2} style={menuItemStyle.titleText}>
+                Guiness
+                {/*Rock Bottom Cask Conditioned Bourbon Chocolate Oatmeal Stout*/}
+            </Text>
+            <Text style={menuItemStyle.keywordText}>
+                #stout #dry #irish
+            </Text>
+            <Text style={menuItemStyle.infoText} numberOfLines={5}>
+                Guinness is an Irish dry stout.
+            </Text>
         </View>
     }
 }
@@ -121,11 +152,13 @@ const menuItemStyle = StyleSheet.create({
         // alignItems:     'flex-start',
         // margin:         10,
         // borderRadius:   10,
-        // borderWidth:    1,
-        height: 100,
+        flex:               0,
+        borderWidth:        1,
+        height:             100,
     },
     primaryMenuItemView: {
-        flexDirection: 'row',
+        flex:           0,
+        flexDirection:  'row',
         justifyContent: 'flex-start',
         alignItems:     'flex-start',
         height:         100,
@@ -143,11 +176,11 @@ const menuItemStyle = StyleSheet.create({
         // borderRadius: 10,
     },
     contentView: {
+        flex:           1,
+        flexDirection:  'row',
         height:         100,
-        marginLeft:     10,
-        marginRight:    10,
-        marginTop:      0,
-        marginBottom:   0,
+        marginLeft:     5,
+        marginRight:    5,
     },
     infoHeader: {
         flexDirection: 'row',
@@ -157,6 +190,7 @@ const menuItemStyle = StyleSheet.create({
     titleText: {
         fontSize: 16,
         fontWeight: "bold",
+        borderBottomWidth: 1,
     },
     priceView: {
         flex:           1,
@@ -193,21 +227,24 @@ class SelectionItem extends Component {
     */
     render = () => {
         return <View>
-            <View style={{flexDirection: 'row', justifyContent: 'flex-start'}}>
-                <Text style={{flex: 1}}>{this.props.name}</Text>
-                <Text style={{flex: 1}}>some text here</Text>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+                <Text style={{width: 80, textAlign: 'center', fontSize: 14}}>2 {this.props.name}</Text>
+                <View style={{flex: 4, flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={{fontSize: 30, textAlign: 'center', marginLeft: 5, marginRight: 5}}>-</Text>
+                    <Slider style={{flex: 3, height: 50}}
+                            value={0}
+                            minimumValue={0}
+                            maximumValue={20}
+                            minimumTrackTintColor='#1fb28a'
+                            maximumTrackTintColor='#d3d3d3'
+                            thumbTintColor='#1a9274'
+                            trackStyle={{height: 10}}
+                            thumbStyle={{height: 20}}
+                            />
+                        <Text style={{fontSize: 30, textAlign: 'center', marginLeft: 5, marginRight: 5}}>+</Text>
+                </View>
+                <Text style={{flex: 1, textAlign: 'center'}}>price</Text>
             </View>
-            {/*
-            <View style={selectionItemStyle.removeItemView}>
-                <Text>-</Text>
-            </View>
-            <View style={selectionItemStyle.textFieldView}>
-                <Text>{this.props.name}</Text>
-            </View>
-            <View style={selectionItemStyle.addItemView}>
-                <Text>+</Text>
-            </View>
-            */}
         </View>
     }
 }
@@ -219,14 +256,5 @@ const selectionItemStyle = StyleSheet.create({
         // justifyContent: 'space-between',
         // alignItems:     'center',
         // marginTop:      20,
-    },
-    removeItemView: {
-        flex: 1,
-    },
-    textFieldView: {
-        flex: 2,
-    },
-    addItemView: {
-        flex: 1,
     },
 })
