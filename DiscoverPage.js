edf78c8f07afe145535b99100cc109b12f028868
Mark Florisson <mark.florisson@cl.cam.ac.uk>
2016-08-10 18:31:46 +0100

    Work on bar cards

--- DiscoverPage.js
+++ DiscoverPage.js
@@ -14,6 +14,7 @@ import Dimensions from 'Dimensions';
 import _ from 'lodash'
 import { observer } from 'mobx-react/native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import LinearGradient from 'react-native-linear-gradient'
 
 import { ImageSwiper } from './ImageSwiper.js'
 import { BarMapView } from './BarMapView.js'
@@ -68,30 +69,90 @@ import { config } from './Config.js'
         const imageStyle = {
             flex: 0,
             height: imageHeight,
-            borderTopLeftRadius: radius,
-            borderTopRightRadius: radius,
+            borderRadius: radius,
+            // borderTopLeftRadius: radius,
+            // borderTopRightRadius: radius,
         }
 
         const { height, width } = Dimensions.get('screen')
 
-        const renderImage = (url, i) =>
-            <TouchableOpacity key={i} onPress={this.handleCardPress}>
-                <Image source={{uri: url}} style={imageStyle} />
-            </TouchableOpacity>
-
-        return <View style={{flex: 0, height: 350, margin: 10, borderTopLeftRadius: radius, borderTopRightRadius: radius}}>
-            <TouchableOpacity onPress={this.handleCardPress}>
-                <Image source={{uri: bar.images[0]}} style={imageStyle} />
+        return <View style={{flex: 0, height: 350, margin: 10, borderRadius: radius, /*borderTopLeftRadius: radius, borderTopRightRadius: radius */}}>
+            <TouchableOpacity onPress={this.handleCardPress} style={{flex: 2, borderRadius: radius}}>
+                <Image source={{uri: bar.images[0]}} style={imageStyle}>
+                    <View style={{flex: 1}}>
+                        {/* Push footer to bottom */}
+                        <View style={{flex: 1}} />
+                        <LinearGradient style={{flex: 1}} colors={['rgba(0, 0, 0, 0.0)', 'rgba(0, 0, 0, 1.0)']}>
+                            <View style={{flex: 1, justifyContent: 'flex-end'}}>
+                                <BarCardFooter bar={bar} />
+                            </View>
+                        </LinearGradient>
+                    </View>
+                </Image>
             </TouchableOpacity>
             {/*
             <ImageSwiper height={250} showButtons={true} width={width - 20}>
                 {bar.images.map(renderImage)}
             </ImageSwiper>
             */}
-            <BarCardFooter bar={bar} />
         </View>
     }
 
+}
+
+const white = 'rgba(255, 255, 255, 0.80)'
+
+@observer class BarInfo extends Component {
+    /* properties:
+        bar: schema.Bar
+    */
+    render = () => {
+        return <View style={{justifyContent: 'flex-end'}}>
+            <View style={{flex: 1, justifyContent: 'flex-end'}}>
+                {/*
+                <Text style={{fontSize: 10, color: "rgba(255, 255, 255, 0.70)"}}>
+                    #pub #food #garden
+                </Text>
+                */}
+                <TimeInfo bar={this.props.bar} />
+            </View>
+            <Text style={{fontSize: 25, color: white}}>
+                {this.props.bar.name}
+            </Text>
+        </View>
+    }
+}
+
+@observer class PlaceInfo extends Component {
+    /* properties:
+        bar: schema.Bar
+    */
+    render = () => {
+        return <View>
+            <TouchableOpacity>
+                <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
+                    <Icon name="map-marker" size={30} color="rgb(181, 42, 11)" />
+                </View>
+                <Text style={{color: white}}>
+                    {this.props.bar.address.city}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    }
+}
+
+@observer class TimeInfo extends Component {
+    /* properties:
+        bar: schema.Bar
+    */
+    render = () => {
+        return <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+            <Icon name="clock-o" size={15} color={white} />
+            <Text style={{marginLeft: 5, fontSize: 10, color: white}}>
+                11.00 - 23.30
+            </Text>
+        </View>
+    }
 }
 
 @observer class BarCardFooter extends Component {
@@ -111,42 +172,52 @@ import { config } from './Config.js'
 
     render = () => {
         const bar = this.props.bar
+        const white = 'rgba(255, 255, 255, 0.80)'
 
-        const footerStyle = {
-            // flexDirection: 'row',
-            // alignItems: 'center',
-            borderWidth: 1,
-            // borderLeftWidth: 1,
-            // borderBottomWidth: 1,
-            // borderRightWidth: 1,
-            borderBottomLeftRadius: 5,
-            borderBottomRightRadius: 5,
-        }
+        // return <BarInfo bar={bar} />
+        return <View style={{flex: 1, flexDirection: 'row', alignItems: 'flex-end'}}>
+            <View style={{flex : 1}}>
+                <BarInfo bar={bar} />
+            </View>
+            <View style={{justifyContent: 'flex-end'}}>
+                <PlaceInfo bar={bar} />
+            </View>
+        </View>
 
-        return <View style={footerStyle}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <View style={{flex: 1}}>
-                    <Text style={{fontSize: 20}}>{bar.name}</Text>
+        return <View style={{justifyContent: 'flex-end'}}>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{flex: 3, justifyContent: 'flex-end'}}>
+                    <Text style={{fontSize: 10, color: "rgba(255, 255, 255, 0.70)"}}>
+                        #pub #food #garden
+                    </Text>
                 </View>
-                <View style={{flex: 1, alignItems: 'center'}}>
-                    <Text>{bar.address.city}</Text>
+                <TouchableOpacity style={{flex: 2}}>
+                    <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
+                        <Icon name="map-marker" size={30} color="rgb(181, 42, 11)" />
+                    </View>
+                </TouchableOpacity>
+                {/*
+                <TouchableOpacity style={{flex: 1}}>
+                    <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
+                        <Icon name="clock-o" size={25} color="rgba(255, 255, 255, 0.60)" />
+                    </View>
+                </TouchableOpacity>
+                */}
+            </View>
+            <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+                <View style={{flex: 3}}>
+                    <Text style={{fontSize: 25, color: white}}>{bar.name}</Text>
                 </View>
+                <View style={{flex: 2, alignItems: 'center'}}>
+                    <Text style={{color: white}}>{bar.address.city}</Text>
+                </View>
+                {/*
                 <View style={{flex: 1, alignItems: 'center'}}>
-                    <Text style={{fontSize: 12, color: 'rgba(0, 0, 0, 0.50)'}}>
+                    <Text style={{fontSize: 12, color: white}}>
                         11.00 - 23.30
                     </Text>
                 </View>
-            </View>
-            <View style={{flexDirection: 'row'}}>
-                <View style={{flex: 1}}>
-                    <Text style={{fontSize: 10, color: "rgba(0, 0, 0, 0.50)"}}>#pub #food #garden</Text>
-                </View>
-                <View style={{flex: 1, alignItems: 'center'}}>
-                    <Icon name="map-marker" size={30} color="rgb(181, 42, 11)" />
-                </View>
-                <View style={{flex: 1, alignItems: 'center'}}>
-                    <Icon name="clock-o" size={20} color="rgba(0, 0, 0, 0.50)" />
-                </View>
+                */}
             </View>
         </View>
 
--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new MapsPackage());
+          new MapsPackage(),
+          new LinearGradientPackage()
+          );
     }
   };
 
