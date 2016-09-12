444cdde28bc91c6103861f632e4a94dd46daae60
Mark Florisson <mark.florisson@cl.cam.ac.uk>
2016-09-12 13:02:32 +0100

    Add script to load drinks.yaml into db

---
+++ loadMenuItems.py
@@ -0,0 +1,59 @@
+import yaml
+import uuid
+from curry import fmap, chunking
+from curry.typing import alias, typeddict, URL
+
+import rethinkdb as r
+
+conn = r.connect()
+menuItems = r.db('qdodger').table('menuItems')
+
+fresh_id = uuid.uuid4
+
+ItemID = alias('ItemID', str)
+TagName = alias('TagName', str)
+
+Drink = typeddict(
+    [ ('name',  str)
+    , ('desc',  str)
+    , ('image', URL)
+    , ('abv',   str)
+    , ('tags',  str)
+    ], name='Drink')
+
+Drinks = { ItemID: Drink }
+
+Contents = typeddict(
+    [ ('drinks', Drinks)
+    ], name='Contents')
+
+def tag_to_tag_id(tag_name):
+    return tag_name
+
+def prepare_item(item_name, item):
+    images = []
+    if 'image' in item:
+        images = fmap(str.strip, item['image'].split('\n'))
+
+    if 'name' not in item or 'desc' not in item or 'tags' not in item:
+        raise ValueError("Invalid item with name " + item_name)
+
+    return {
+        'id':       item_name,
+        'name':     item['name'],
+        'desc':     item['desc'],
+        'images':   images,
+        'tags':     fmap(tag_to_tag_id, filter(bool, item['tags'].split())),
+    }
+
+def load_items_from_file(contents, dry_run=False):
+    drink_list = contents['drinks']
+    results = [prepare_item(item_name, item)
+                   for item_name, item in drink_list.items()]
+    if not dry_run:
+        menuItems.delete()
+        menuItems.insert(results).run(conn)
+
+if __name__ == '__main__':
+    yaml_file = yaml.load(open('drinks.yaml'))
+    load_items_from_file(yaml_file, dry_run=False)
