13df4c071ff995f975f952c433d313bc5374e6d4
Mark Florisson <mark.florisson@cl.cam.ac.uk>
2016-09-23 17:09:37 +0100

    Make hearting more performant

--- Fav.js
+++ Fav.js
@@ -3,7 +3,7 @@ import { View, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import { PureComponent } from './Component.js'
-import { observable, action, autorun } from 'mobx'
+import { observable, action, autorun, computed } from 'mobx'
 import { observer } from 'mobx-react/native'
 
 import { loginStore } from './Store.js'
@@ -56,14 +56,21 @@ export const favStore = new FavStore()
 
 /* Components */
 
-@observer export class Heart extends PureComponent {
+@observer
+export class Heart extends PureComponent {
     /* properties:
         id: ID
         style: style object
         iconSize: int
+        isSelected: () => Bool
     */
+
+    @computed get selected() {
+        return this.props.isSelected()
+    }
+
     render = () => {
-        const iconName = this.props.selected ? "heart" : "heart-o"
+        const iconName = this.selected ? "heart" : "heart-o"
         return <TouchableOpacity onPress={this.props.toggle}>
             <View style={this.props.style}>
                 <Icon
@@ -77,18 +84,20 @@ export const favStore = new FavStore()
     }
 }
 
-@observer export class FavBarContainer extends PureComponent {
+@observer
+export class FavBarContainer extends PureComponent {
     render = () => {
         const toggle = () => favStore.toggleFavBar(this.props.barID)
-        const selected = favStore.isFavBar(this.props.barID)
-        return <Heart selected={selected} toggle={toggle} {...this.props} />
+        const isSelected = () => favStore.isFavBar(this.props.barID)
+        return <Heart isSelected={isSelected} toggle={toggle} {...this.props} />
     }
 }
 
-@observer export class FavItemContainer extends PureComponent {
+@observer
+export class FavItemContainer extends PureComponent {
     render = () => {
         const toggle = () => favStore.toggleFavItem(this.props.menuItemID)
-        const selected = favStore.isFavItem(this.props.menuItemID)
-        return <Heart selected={selected} toggle={toggle} {...this.props} />
+        const isSelected = () => favStore.isFavItem(this.props.menuItemID)
+        return <Heart isSelected={isSelected} toggle={toggle} {...this.props} />
     }
 }
