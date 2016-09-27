3c8c438769800db2ee620c423a090b1460e45796
Mark Florisson <mark.florisson@cl.cam.ac.uk>
2016-09-27 18:43:48 +0100

    Use cloud instance

--- Host.js
+++ Host.js
@@ -1,6 +1,7 @@
 export var HOST : String
 // HOST = 'http://192.168.0.6:5000'
-HOST = 'http://10.248.100.107:5000'
+// HOST = 'http://10.248.100.107:5000'
+HOST = 'http://104.199.33.201' // gcloud instance
 // HOST = 'http://192.168.0.28:5000'
 // HOST = 'http://172.24.176.169:5000'
 // HOST = 'http://localhost:5000/graphql'
