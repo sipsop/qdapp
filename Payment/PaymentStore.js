42244eec03fbfd9467c689f3c1707aa704167ae8
Mark Florisson <mark.florisson@cl.cam.ac.uk>
2016-10-18 17:39:33 +0100

    Remove credit cards used for testing

--- Payment/PaymentStore.js
+++ Payment/PaymentStore.js
@@ -114,9 +114,9 @@ const exampleCard = (cardType : String, cardNumber : String) : Card => {
 if (_.DEV) {
     const addExample = (cardType, cardNumber) => paymentStore.addCard(exampleCard(cardType, cardNumber))
     setTimeout(() => {
-        addExample('Visa',              '4111111111111111')
-        addExample('MasterCard', 	    '5500000000000022')
-        addExample('American Express', 	'340000000000033')
+        // addExample('Visa',              '4111111111111111')
+        // addExample('MasterCard', 	    '5500000000000022')
+        // addExample('American Express', 	'340000000000033')
         // addExample("Diner's Club",      '30000000000044')
         // addExample('Carte Blanche',     '30000000000055')
         // addExample('Discover', 	        '6011000000000066')
