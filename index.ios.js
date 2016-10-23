18c7bcff6319af4f7a2c66dea97ed42a290f7b5b
Richa Chandola <chandola.richa@gmail.com>
2016-10-23 12:26:43 +0100

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,35 +1,6 @@
 import React, { Component } from 'react'
-import {
-  AppRegistry,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  ListView,
-  Platform,
-  PickerIOS,
-  TouchableOpacity,
-} from 'react-native'
-import Dimensions from 'Dimensions'
-import Swiper from 'react-native-swiper'
-import { observable, computed, autorun } from 'mobx'
-import { observer } from 'mobx-react/native'
-import * as _ from './Curry.js'
-
-
-// import PickerAndroid from 'react-native-picker-android';
-// import MapView from 'react-native-maps'
-import { Main } from './Main.js'
-import { store } from './Store.js'
-// import rootNode from './RelayTest.js'
-import { MapView } from './Maps/MapView.js'
-
-
-class App extends Component {
-    render = () => {
-        return <Main />
-    }
-}
+import { AppRegistry } from 'react-native'
+import { main, App } from './Main.js'
 
 AppRegistry.registerComponent('QDodger', () => App);
+main()
