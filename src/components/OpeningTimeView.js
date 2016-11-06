27150423e9d2bc2981bd2e74242897dd378d9141
Damir Kozica <damirkzc@gmail.com>
2016-11-06 22:14:29 +0100

    empty render method of old openingtimeview

--- src/components/OpeningTimeView.js
+++ src/components/OpeningTimeView.js
@@ -23,17 +23,8 @@ export class OpeningTimeView extends PureComponent {
     */
 
     render = () => {
-        const textStyle = this.props.textStyle
-        const openingTime = this.props.openingTime
-        if (!openingTime) {
-            return <T style={textStyle}>Unknown</T>
-        }
         return (
-            <View style={styles.row}>
-                <Time style={textStyle} time={openingTime.open} />
-                <T style={textStyle}> - </T>
-                <Time style={textStyle} time={openingTime.close} />
-            </View>
+          <View />
         )
     }
 }
