37a6f792fae61e395d2e6f53d8dde713f866913d
Damir Kozica <damirkzc@gmail.com>
2016-11-13 16:51:29 +0100

    add funcs for new modal in root

--- src/App.js
+++ src/App.js
@@ -8,26 +8,37 @@ import {
 import { Router } from './Router'
 import { Main } from './Main'
 import { OpeningTimesModal } from './components/modals/OpeningTimesModal'
+import { MenuItemModal } from './components/modals/MenuItemModal'
 import { modalStore } from './model/store'
 
 @observer
 export class App extends Component {
+
     closeOpeningTimesModal = () => {
         modalStore.closeOpeningTimesModal()
     }
+    closeMenuItemModal = () => {
+        modalStore.closeMenuItemModal()
+    }
+
     render = () => {
         const showOpeningTimes = modalStore.showOpeningTimesModal
-        if (Platform.OS === 'android')
+        const showMenuItemModal = modalStore.showMenuItemModal
+
+        if (Platform.OS === 'android') {
             return (
               <View style={{flex: 1}}>
                 <Main />
                 <OpeningTimesModal onClosedProp={this.closeOpeningTimesModal} isVisible={showOpeningTimes} />
+                <MenuItemModal onClosedProp={this.closeOpeningTimesModal} isVisible={showMenuItemModal} />
               </View>
             )
+        }
         return (
             <NavigationProvider router={Router}>
                 <StackNavigation initialRoute={Router.getRoute('main')} />
                 <OpeningTimesModal onClosedProp={this.closeOpeningTimesModal} isVisible={showOpeningTimes} />
+                <MenuItemModal onClosedProp={this.closeMenuItemModal} isVisible={showMenuItemModal} />
             </NavigationProvider>
         )
     }
