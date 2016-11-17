8fa95cbbb8f5e782a0db574a644623137c379758
Mark Florisson <mark.florisson@cl.cam.ac.uk>
2016-11-17 16:00:42 +0000

    Start on push notifications with react-native-fcm

--- android/app/src/main/java/com/qdodger/MainApplication.java
+++ android/app/src/main/java/com/qdodger/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.auth0.lock.react.LockReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FIRMessagingPackage(),
             new VectorIconsPackage(),
             new MapsPackage(),
             new LockReactPackage(),
--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import {
   NavigationProvider,
   StackNavigation
 } from '@exponent/ex-navigation'
+import FCM from 'react-native-fcm'
 
 import { Router } from './Router'
 import { Main } from './Main'
 import { OpeningTimesModal } from './components/modals/OpeningTimesModal'
 import { MessageListModal } from '/components/messages/MessageListModal'
 import { modalStore } from './model/store'
+import * as _ from '/utils/curry'
+
+const { log, assert } = _.utils('/components/App')
 
 @observer
 export class App extends PureComponent {
@@ -31,6 +35,61 @@ export class App extends PureComponent {
 
 @observer
 class MainApp extends PureComponent {
+
+    componentDidMount = () => {
+        FCM.requestPermissions() // for iOS
+        FCM.getFCMToken().then(token => {
+            log("GOT FCM TOKEN", token)
+            // store fcm token in your server
+        })
+        this.notificationUnsubscribe = FCM.on('notification', (notification) => {
+            // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+            log("GOT A NOTIFICATION!")
+            log(notification)
+            if (notification.local_notification) {
+                //this is a local notification
+                log("LOCAL NOTIFICATION")
+            }
+            if (notification.opened_from_tray) {
+                //app is open/resumed because user clicked banner
+                log("OPENED FROM TRAY")
+            }
+        })
+        this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+            console.log(token)
+            // fcm token may not be available on first load, catch it here
+        })
+
+        // Test local push notification
+        FCM.presentLocalNotification({
+            id: "UNIQ_ID_STRING",                               // (optional for instant notification)
+            title: "My Notification Title",                     // as FCM payload
+            body: "My Notification Message",                    // as FCM payload (required)
+            sound: "default",                                   // as FCM payload
+            priority: "high",                                   // as FCM payload
+            click_action: "ACTION",                             // as FCM payload
+            badge: 10,                                          // as FCM payload IOS only, set 0 to clear badges
+            number: 10,                                         // Android only
+            ticker: "My Notification Ticker",                   // Android only
+            auto_cancel: true,                                  // Android only (default true)
+            large_icon: "ic_launcher",                           // Android only
+            icon: "ic_notification",                            // as FCM payload
+            big_text: "Show when notification is expanded",     // Android only
+            sub_text: "This is a subText",                      // Android only
+            color: "red",                                       // Android only
+            vibrate: 300,                                       // Android only default: 300, no vibration if you pass null
+            tag: 'some_tag',                                    // Android only
+            group: "group",                                     // Android only
+            my_custom_data:'my_custom_field_value',             // extra data you want to throw
+            lights: true,                                       // Android only, LED blinking (default false)
+            show_in_foreground                                  // notification when app is in foreground (local & remote)
+        })
+    }
+
+    closeOpeningTimesModal = () => {
+        modalStore.closeModal()
+    }
+
     render = () => {
         if (Platform.OS === 'android') {
             return (
