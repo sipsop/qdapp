3df5ea6474fb111c8884da4158b119e6a9cca988
Damir Kozica <damirkzc@gmail.com>
2016-11-29 23:16:28 +0100

    adding observables and actions into the modalstore for new modal

--- src/model/modalstore.js
+++ src/model/modalstore.js
@@ -2,34 +2,42 @@ import { observable, computed, action } from 'mobx'
 
 class ModalStore {
   @observable showOpeningTimesModal = false
+  @observable showBookingRequestModal = false
 
     initialize = () => {
     }
 
     getState = () => {
         return {
-            showOpeningTimesModal: this.showOpeningTimesModal
+            showOpeningTimesModal: this.showOpeningTimesModal,
+            showBookingRequestModal: this.showBookingRequestModal,
         }
     }
 
     emptyState = () => {
         return {
-            showOpeningTimesModal: false
+            showOpeningTimesModal: false,
+            showBookingRequestModal: false,
         }
     }
 
-    @action openModal = () => {
+    @action openOpeningModal = () => {
         this.showOpeningTimesModal = true
     }
-
-    @action closeModal = () => {
+    @action closeOpeningModal = () => {
         this.showOpeningTimesModal = false
+    }
+
+    @action openBookingRequestModal = () => {
+        this.showBookingRequestModal = true
+    }
+    @action closeBookingRequestModal = () => {
+        this.showBookingRequestModal = false
     }
 
     @computed get getModalStatus () {
         return this.showOpeningTimesModal
     }
-
 }
 
 export const modalStore = new ModalStore()
