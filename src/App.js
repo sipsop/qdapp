4c9afcc57c25c759dd7beccab5e1ee67f9fa859a
Damir Kozica <damirkzc@gmail.com>
2016-11-29 23:13:26 +0100

    adding modal in root

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ import {
 import { Router } from './Router'
 import { Main } from './Main'
 import { OpeningTimesModal } from './components/modals/OpeningTimesModal'
+import { RequestBookingModal } from './components/modals/RequestBookingModal'
 import { modalStore } from './model/store'
 
 @observer
 export class App extends Component {
     closeOpeningTimesModal = () => {
-        modalStore.closeModal()
+        modalStore.openOpeningModal()
+    }
+    closeRequestBookingModal = () => {
+        modalStore.closeBookingModal()
+    }
+    componentDidMount() {
+        console.disableYellowBox = true;
     }
     render = () => {
         const showOpeningTimes = modalStore.showOpeningTimesModal
+        const showBookingModal = modalStore.showBookingRequestModal
         if (Platform.OS === 'android')
             return (
               <View style={{flex: 1}}>
@@ -28,6 +36,7 @@ export class App extends Component {
             <NavigationProvider router={Router}>
                 <StackNavigation initialRoute={Router.getRoute('main')} />
                 <OpeningTimesModal onClosedProp={this.closeOpeningTimesModal} isVisible={showOpeningTimes} />
+                <RequestBookingModal onClosedProp={this.closeBookingModal} isVisible={showBookingModal} />
             </NavigationProvider>
         )
     }
